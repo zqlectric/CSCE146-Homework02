b9d16b69b1d866e06ecf89cff463c136f16c046d
Zach Goldberg <64557984+zqlectric@users.noreply.github.com>
2024-12-04 21:34:44 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+/*
+ * Written by Zachary Goldberg
+ */
+public class main {
+	public static helper hper = new helper();
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the Video Game Database!");
+		hper.textMessage();
+	}
+
+}
